9c04622ab4560d531610b9a3c33963cb85f5ef87
Andrés Miguel Zaragoza Quintero <77632061+andreszaragoza@users.noreply.github.com>
2024-12-25 13:04:52 +0100

    updatee

---
+++ Analisis_Andres/scripts/data_cleaning.py
@@ -0,0 +1,22 @@
+# Función para convertir texto a objetos Python
+def convert_to_json(data):
+    try:
+        return ast.literal_eval(data)
+    except (ValueError, SyntaxError):
+        return None
+
+# Función para desanidar columnas
+def desanidar_columna(dataframe, column_name):
+    """
+    Convierte cadenas JSON-like, explota listas y normaliza diccionarios en una columna específica.
+    """
+    # Convertir las cadenas JSON-like a estructuras Python
+    dataframe[column_name] = dataframe[column_name].dropna().apply(convert_to_json)
+    
+    # Explode: Expande listas en múltiples filas
+    exploded = dataframe[column_name].explode().dropna()
+    
+    # Normalizar diccionarios si están presentes
+    desanidado = json_normalize(exploded)
+    
+    return desanidado
