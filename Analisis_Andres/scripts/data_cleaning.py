015a4479c6872b31a0aae9ff32cf44b808d376b8
Andrés Miguel Zaragoza Quintero <77632061+andreszaragoza@users.noreply.github.com>
2025-01-06 16:00:27 +0100

    nuevo_update

--- Analisis_Andres/scripts/data_cleaning.py
+++ Analisis_Andres/scripts/data_cleaning.py
@@ -1,49 +1,55 @@
-from pandas import json_normalize
+import ast
+import pandas as pd
 
-# Función para convertir texto a objetos Python
+
 def convert_to_json(data):
     try:
         return ast.literal_eval(data)
     except (ValueError, SyntaxError):
         return None
 
-# Función para desanidar columnas
-def desanidar_columna(dataframe, column_name):
+def unnest_column(df, column_name):
     """
     Convierte cadenas JSON-like, explota listas y normaliza diccionarios en una columna específica.
     """
-    dataframe[column_name] = dataframe[column_name].dropna().apply(convert_to_json)
-    
-    exploded = dataframe[column_name].explode().dropna()
-    
-    desanidado = json_normalize(exploded)
-    
-    return desanidado
+    df[column_name] = df[column_name].dropna().apply(convert_to_json)
 
-# Procesar la columna 'genres' para extraer los nombres de los géneros
+    exploded = df[column_name].explode().dropna()
+
+    unnested_column = pd.json_normalize(exploded) 
+
+    return unnested_column
+
+
 def extract_genre_names(genres):
+    """
+    Extraer el nombre de cada género
+    """
     try:
-        # Convertir el texto a lista de diccionarios
         genres_list = ast.literal_eval(genres)
-        # Extraer solo los nombres
         return [genre['name'] for genre in genres_list]
     except (ValueError, TypeError):
         return []
-    
-# Procesar columnas categóricas que contienen listas o estructuras complejas
-def preprocess_column(column):
-    if df[column].dtype == 'object' and df[column].apply(lambda x: isinstance(x, list)).any():
-        # Si la columna contiene listas, expandirlas
-        return df[column].explode().dropna().nunique()
-    else:
-        # Si no, calcular directamente la cardinalidad
-        return df[column].nunique()
 
-# Función para procesar y validar las etiquetas
-def process_tags(tag_string):
+
+def preprocess_column(df, column_name):
+    """
+    
+    """
+    if df[column_name].dtype == 'object' and df[column_name].apply(lambda x: isinstance(x, list)).any():
+        return df[column_name].explode().dropna().nunique()
+    else:
+        return df[column_name].nunique()
+
+
+def extract_tag_names(tag_string):
+    """
+    Extraer el nombre de cada etiqueta
+    """
     try:
-        # Intentar evaluar el string como una lista
-        return [tag['name'] for tag in ast.literal_eval(tag_string)] if pd.notnull(tag_string) else []
+        if pd.notnull(tag_string):
+            return [tag['name'] for tag in ast.literal_eval(tag_string)]
+        else:
+            return []
     except (ValueError, SyntaxError, TypeError):
-        # Retornar una lista vacía si falla
         return []
